f06c401262a18c2060d625cdee2e5de32be02ac5
SerebryakovaIA <ia.serebr@gmail.com>
2024-07-03 18:08:31 +0300

    Проверьте, пожалуйста, домашнее задание №7.

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,59 @@
 public class Main {
+    public static double countProfitForMonth(double totalSum, double monthPayment, double interestRate){  //вклад с ежемесячным пополнением
+        totalSum += monthPayment + ((totalSum/100)*interestRate);
+        return totalSum;
+    }
+
+    public static double countProfitForMonth(double totalSum, double percent){  //вклад без пополнения
+        totalSum += totalSum * percent;
+        return totalSum;
+    }
+
     public static void main(String[] args) {
 
-        // Task 1
-        int total = 0;
-        int payment = 15000;
+        // Task 1 (12% годовых)
+        double totalSum = 0.0;
+        double monthPayment = 15000.0;
         int month = 1;
-        while (total < 2_459_000) {
-            total += payment;
+        while (totalSum < 2_459_000) {
+            totalSum = countProfitForMonth(totalSum, monthPayment, 0.12);
+            System.out.print(String.format("Месяц %d, сумма накоплений равна %.2f рублей\n", month, totalSum));
             month++;
         }
-        System.out.print(String.format("Месяц %d, сумма накоплений равна %d рублей\n", month, total));
+        System.out.println();
 
 
         // Task 2
-        int iterator = 0;
-        while (iterator < 10) {
-            iterator++;
-            System.out.print(iterator + " ");
+        int counter = 0;
+        while (counter < 10) {
+            counter++;
+            System.out.print(counter + " ");
         }
         System.out.println();
-        for (iterator = 10; iterator > 0; iterator--) {
-            System.out.print(iterator + " ");
+        for (counter = 10; counter > 0; counter--) {
+            System.out.print(counter + " ");
         }
+        System.out.println();
 
 
         // Task 3
-        double birthRate = 0.017;  // рождаемость
-        double mortalityRate = 0.008;  // смертность
+        final double BIRTH_RATE = 0.017;  // рождаемость
+        final double MORTALITY_RATE = 0.008;  // смертность
         int populationSize = 12_000_000;  // численность населения
-        int year = 0;
-        while (year < 10) {
-            populationSize = (int) (populationSize + (populationSize * birthRate) - (populationSize * mortalityRate));
+        int year = 1;
+        while (year <= 10) {
+            populationSize = (int) (populationSize + (populationSize * BIRTH_RATE) - (populationSize * MORTALITY_RATE));
+            System.out.println(String.format("Год %d, численность населения составляет %d.", year, populationSize));
             year++;
         }
-        System.out.println(String.format("\nГод %d, численность населения составляет %d.", year, populationSize));
 
 
         // Task 4
-        double percent = 0.07;
+        final double INTEREST_RATE = 0.07;
         double sum = 15000;
         month = 1;
         while (sum < 12_000_000) {
-            sum += sum * percent;
+            sum = countProfitForMonth(sum, INTEREST_RATE);
             System.out.print(String.format("\nМесяц %d, сумма накоплений составляет %.2f.", month, sum));
             month++;
         }
@@ -49,13 +61,12 @@ public class Main {
 
 
         // Task 5
-        percent = 0.07;
         sum = 15000;
         month = 1;
         while (sum < 12_000_000) {
-            sum += sum * percent;
+            sum = countProfitForMonth(sum, INTEREST_RATE);
             if (month % 6 == 0) {
-                System.out.print(String.format("\nМесяц %d, сумма накоплений составляет %.2f.", month, sum));
+                System.out.printf("\nМесяц %d, сумма накоплений составляет %.2f.", month, sum);
             }
             month++;
         }
@@ -63,14 +74,13 @@ public class Main {
 
 
         // Task 6
-        percent = 0.07;
         sum = 15000;
         month = 1;
         if (month < (9 * 12) + 1) {
             while (sum < 12_000_000) {
-                sum += sum * percent;
+                sum = countProfitForMonth(sum, INTEREST_RATE);
                 if (month % 6 == 0) {
-                    System.out.print("\nМесяц " + month + ", сумма накоплений составляет " + sum);
+                    System.out.printf("\nМесяц %d, сумма накоплений составляет %.2f.", month, sum);
                 }
                 month++;
             }
@@ -88,17 +98,17 @@ public class Main {
 
         // Task 8
         int currentYear = 0;
-        iterator = 79;
+        counter = 79;
         int beginningOfPeriod = 2024 - 200;
         int endOfPeriod = 2024 + 100;
 
         do {
-            currentYear += iterator;
+            currentYear += counter;
         } while (currentYear < beginningOfPeriod);
 
         while (currentYear < endOfPeriod) {
             System.out.println(currentYear);
-            currentYear += iterator;
+            currentYear += counter;
         }
     }
 }
